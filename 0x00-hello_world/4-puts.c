c2d1168c83eff3c4a6292c5136bdb7561ec10152
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-07 07:27:22 -0700

    hh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
-/* main fuction prints the statement using puts in C */
-int main (void){
+int main(void){
 	/*printing using puts function in C*/
-       char str="\"Programming is like building a multilingual puzzle\n"	
+       char str[52]="\"Programming is like building a multilingual puzzle\n"	
 	puts(str);
 	return 0;
 }
