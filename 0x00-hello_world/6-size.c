c7cf9af55e623a6fed6b46d8e879d5648eb57ab1
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-07 12:36:07 -0700

    ff

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - printf "the size of a char:\n ", sizeof(char)
+ * main- printf "the size of an int:\n ", sizeof(int))
+ * main - printf "the size of a long int:\n ", sizeof(int)
+ * main -printf "the size of a long long int:\n ", sizeof(long)
+ * main - printf "the size of a float:\n ", sizeof(float)
+ * void: there's nothing here
+ * Return: 0 always
+*/
+int main(void)
+{
+printf("the size of a char:\n ", sizeof(char));
+printf("the size of an int:\n ", sizeof(int));
+printf("the size of a long int:\n ", sizeof(int));
+printf("the size of a long long int:\n ", sizeof(long));
+printf("the size of a float:\n ", sizeof(float));
+return (0);
+}
