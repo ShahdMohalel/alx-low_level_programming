5afec49e73e3d8579fd7ed942d9f997f67212646
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-07 13:12:06 -0700

    ddd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,12 @@ int main(void)
 {
 char a;
 int b;
-long c;
+double c;
 float d;
 printf("Size of a char: %zu byte(s)\n", sizeof(a));
 printf("Size of an int: %zu byte(s)\n", sizeof(b));
 printf("Size of a long int: %zu byte(s)\n", sizeof(b));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(c+b));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(c));
 printf("Size of a float: %zu byte(s)\n", sizeof(d));
 return (0);
 }
