d6f1c37b3d2bb6cf24717600ed97ebcca5de486a
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-08 13:13:24 -0700

    dd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - putchar(ch[i])
+ * void: no parameters
+ * Return: 0 always
+*/
+int main(void)
+{
+char ch[];
+ch[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+for (int i = 0; i < 26; i++)
+{
+putchar(ch[i] + "\n");
+}
+}
