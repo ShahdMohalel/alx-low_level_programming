4b45affd00b14ade482c5ce48b7e5c52c3a5200f
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-08 15:17:49 -0700

    dd

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - putchar('0'+ i)
+ * void: no parameters
+ * Return: 0 always
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+}
+putchar('\n');
+}
