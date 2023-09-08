043b9160ba0ea66e62364367a99b4b990990f2db
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-08 10:39:15 -0700

    ddd

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+/*
+*main - if condition
+{
+printf("%d is positive", n);
+}
+else if(n<0)
+{
+printf("%d is negative", n);
+}
+else
+{
+printf("%d is zero", n);
+}
+*void - take no parameters
+*Return - 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive", n);
+}
+else if (n < 0)
+{
+printf("%d is negative", n);
+}
+else
+{
+printf("%d is zero", n);
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+learning if_else and while loop
