0784daec4524492703dbc17bededb52afcce7f23
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-08 10:43:00 -0700

    ss

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,15 @@
 /*
 *main - if condition
 {
-printf("%d is positive", n);
+printf("%d is positive\n", n);
 }
 else if(n<0)
 {
-printf("%d is negative", n);
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 *void - take no parameters
 *Return - 0
@@ -24,15 +24,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive", n);
+printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
-printf("%d is negative", n);
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is zero", n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
