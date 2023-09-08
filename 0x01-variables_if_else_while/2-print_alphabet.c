ac8780a12512a6d830d5bcc1cc97ee04492c67ca
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-08 13:44:54 -0700

    ddd

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,10 @@
 */
 int main(void)
 {
-char ch[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-int i;
-for (i = 0; i < 26; i++)
+char i;
+for (i = 'a'; i <= 'z'; i++)
 {
-putchar(ch[i]);
+putchar(i);
 }
 putchar('\n');
 return (0);
