bd58e6515034c9b1b588be30ac7f14370aae803d
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-08 17:09:30 -0700

    ff

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - putchar(01-89)
+ * void: no parameters
+ * Return: 0 always
+*/
+int main(void)
+{
+for (int i = 0; i < 9; i++)
+{
+for (int j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
