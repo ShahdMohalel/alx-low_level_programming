3181719da1c9faa816e2b561afa5cefb4f7638c7
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-08 17:29:43 -0700

    ff

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,9 @@ int j;
 int k;
 for (i = 0; i < 8; i++)
 {
-for (j = 1; j < 9; j++)
+for (j = i + 1; j < 9; j++)
 {
-for (k = 2; k < 10; k++)
+for (k = j + 1; k < 10; k++)
 {
 putchar(i + '0');
 putchar(j + '0');
