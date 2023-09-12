dc352a5bc9226945ed1ab63c355d3b82a386abb4
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-12 13:44:18 -0700

    dd

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_sign - check numbers sign
+ * @n: The parameter 'n' represents the number to be checked
+ * Return: 1 if true / 0 if false
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+putchar(',');
+putchar(' ');
+putchar(1, '0');
+return (1);
+}
+else if (n < 0)
+{
+putchar('-');
+putchar(',');
+putchar(' ');
+putchar(1, '0');
+return (1);
+}
+else
+{
+putchar('0');
+putchar(',');
+putchar(' ');
+putchar(0, '0');
+return (0);
+}
+}
