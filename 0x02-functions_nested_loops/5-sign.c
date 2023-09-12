5947e9637bdcc007e28922ff1061342097bfa8f0
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-12 14:05:30 -0700

    dd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,18 +9,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
-putchar('1');
 putchar('\n');
-return (1);
+return (+1);
 }
 else if (n < 0)
 {
-putchar('-');
-return (1);
+putchar('\n');
+return (-1);
 }
 else
 {
-return (0);
+putchar('\n');
+return (00);
 }
 }
