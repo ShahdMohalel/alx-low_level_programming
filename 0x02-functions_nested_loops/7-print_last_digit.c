088de02a44b506fc7bb0a3a3b4b1085229163682
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-12 15:01:10 -0700

    ss

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - return last didgit if a number
+ * @e: The parameter 'e' represents the number
+ * Return: (e % 10)
+*/
+int print_last_digit(int e)
+{
+return (e % 10);
+}
