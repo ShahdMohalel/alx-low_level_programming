c27cebab93a103887daca4c8741d3f810e0cc950
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-12 13:17:32 -0700

    dd

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _isalpha - check if character is an alphabetic character
+ * @c: The parameter 'c' represents ASCII number of an alphabetic character
+ * Return: 1 if true / 0 if false
+*/
+int _isalpha(int c)
+{
+if (c <= 122 && 97 <= c || c <= 90 && 65 <= c)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
