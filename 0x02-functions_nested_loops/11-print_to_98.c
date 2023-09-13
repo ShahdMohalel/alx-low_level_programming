d0fe1f56571450bcd519d910109003943687eb78
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-13 09:10:05 -0700

    dd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,15 @@ void print_to_98(int n)
 int i;
 for (i = n; i <= 98; i++)
 {
-putchar('0' + i);
-putchar(',');
-putchar(' ');
+if (i != 98)
+{
+printf("%d, ", i);
 }
-putchar('\n');
+else
+{
+printf("%d", i);
+}
+}
 return;
 }
+
