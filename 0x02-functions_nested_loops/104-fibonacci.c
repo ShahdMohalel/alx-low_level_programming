831762c969d46d8778e07f73dd5eba6bd4c4a27a
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-13 16:58:06 -0700

    dd

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,29 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * main - printf("%lu\n",even_sum)
+ * main - printf("%d, ", Fibonacci[i])
  * void: no parameters
  * Return: 0 always
 */
 int main(void)
 {
 unsigned long int fib[50];
-unsigned long even_sum;
 fib[0] = 1;
 fib[1] = 2;
-for (i = 2; fib[i] < 4000000; i++)
+for (i = 2; i < 98; i++)
 {
 fib[i] = fib[i - 1] + fib[i - 2];
 }
-for (i = 0; i < 50; i++)
+for (i = 0; i < 98; i++)
 {
-if (fib[i] % 2 == 0)
+if (i < 97)
 {
-even_sum += fib[i];
+printf("%lu, ", fib[i]);
+}
+else
+{
+printf("%lu", fib[i]);
 }
 }
-printf("%lu\n", even_sum);
+printf("\n");
 return (0);
-}
