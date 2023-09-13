60e1d68be362928b2c4c549974d70a48cca931e7
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-13 16:23:10 -0700

    dd

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,12 @@
 */
 int main(void)
 {
-int Fib[50];
+int fib[50];
 fib[0] = 1;
 fib[1] = 2;
 for (i = 2; i < 50; i++)
 {
-Fib[i] = fib[i - 1] + fib[i - 2];
+fib[i] = fib[i - 1] + fib[i - 2];
 }
 for (i = 0; i < 50; i++)
 {
