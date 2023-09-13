6dd110e2549e98b26dc878ced2ace951d0be987d
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-13 09:50:45 -0700

    dd

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * times_table - print times table.
+ * void: no parameters "void".
+ * Return: no return.
+*/
+void times_table(void)
+{
+int i;
+int j;
+int table;
+i = 0;
+while (i < 10)
+{
+j = 0;
+while (j < 10)
+{
+table = i *j;
+printf("%d, ", table);
+j++;
+}
+printf("\n");
+i++;
+}
+return;
+}
