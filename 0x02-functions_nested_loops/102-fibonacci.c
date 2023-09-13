f05cdaedd5a478782eaa0fbd9cf346e96add26c9
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-13 15:32:15 -0700

    dd

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - printf("%d, ", Fibonacci[i])
+ * void: no parameters
+ * Return: 0 always
+*/
+int main(void)
+{
+int Fibonacci[50];
+for (i = 1; i < 50; i++)
+{
+Fibonacci[i] = i + (i - 1);
+printf("%d, ", Fibonacci[i]);
+return (0);
+}
