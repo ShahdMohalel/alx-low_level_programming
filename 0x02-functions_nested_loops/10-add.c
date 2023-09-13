2a094517aae0b2a69b604e416899d7d9d3b57932
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-13 08:15:33 -0700

    ss

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * add - adds two numbers
+ * 'a':an integer number
+ * 'b':an integer number
+ * Return: "result" of the addition 'a'+'b'
+*/
+int add(int a, int b)
+{
+int result;
+result = a + b;
+return (result);
+}
