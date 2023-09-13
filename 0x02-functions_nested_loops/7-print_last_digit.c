ef586640f7cb93326979068dd944020c0c1a4c54
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-13 11:08:18 -0700

    dd

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,12 +5,11 @@
  * @e: The parameter 'e' represents the number
  * Return: (e % 10)
 */
-void print_last_digit(int e)
+int print_last_digit(int e)
 {
 int end;
 end = e % 10;
 putchar(end + '0');
-putchar(end + '0');
 putchar('\n');
-return;
+return (end);
 }
