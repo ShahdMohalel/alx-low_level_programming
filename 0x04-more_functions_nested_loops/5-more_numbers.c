f67160c6cb88679669f24a21f7c956bc2ce7b4c5
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-14 14:01:50 -0700

    dd

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
  * more_numbers - print numbers
  * void: no parameters.
- * Return: no return
+ * Return: no return.
 */
 void more_numbers(void)
 {
@@ -14,8 +14,8 @@ for (i = 0; i <= 10; i++)
 for (j = 0; j <= 14; j++)
 {
 _putchar('0' + j);
-_putchar('\n');
 }
+_putchar('\n');
 }
 _putchar('\n');
 return;
