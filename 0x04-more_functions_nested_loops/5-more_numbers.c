f84ad5e89bd3b144ac473a3ad53b42c05c9f9b35
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-14 13:59:21 -0700

    dd

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * more_numbers - print numbers
+ * void: no parameters.
+ * Return: no return
+*/
+void more_numbers(void)
+{
+int i;
+int j;
+for (i = 0; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+_putchar('0' + j);
+_putchar('\n');
+}
+}
+_putchar('\n');
+return;
+}
