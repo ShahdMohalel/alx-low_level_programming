51bd3c8e9c663173933c381103ab3e3fd01594bc
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-14 13:12:54 -0700

    dd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * mul - mult two numbers.
+ * @a: The parameter 'a' represents the number to be mult.
+ * @b: The parameter 'b' represents the number to be mult.
+ * Return: result.
+*/
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
