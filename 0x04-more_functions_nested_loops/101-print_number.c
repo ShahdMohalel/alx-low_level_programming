9a5125aecc81c259a692bb6e2348040843993721
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-15 10:39:23 -0700

    dd

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,31 +11,31 @@ int m, l;
 if (n < 0)
 {
 n = n / -1;
-putchar('-');
+_putchar('-');
 }
 if (n >= 0 && n <= 9)
 {
-putchar('0' + n);
+_putchar('0' + n);
 }
 else if (n >= 10 && n <= 99)
 {
-putchar('0' + n / 10);
-putchar('0' + n % 10);
+_putchar('0' + n / 10);
+_putchar('0' + n % 10);
 }
 else if (n >= 100 && n <= 999)
 {
-putchar('0' + n / 100);
+_putchar('0' + n / 100);
 m = n % 100;
-putchar('0' + m / 10);
-putchar('0' + n % 10);
+_putchar('0' + m / 10);
+_putchar('0' + n % 10);
 }
 else if (n >= 1000 && n <= 9999)
 {
-putchar('0' + n / 1000);
+_putchar('0' + n / 1000);
 l = n / 100;
-putchar('0' + l % 10);
+_putchar('0' + l % 10);
 m = n % 100;
-putchar('0' + m / 10);
-putchar('0' + n % 10);
+_putchar('0' + m / 10);
+_putchar('0' + n % 10);
 }
 }
