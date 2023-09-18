e25f085357845772e2e25d59046bec61183f4e45
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-18 08:31:28 -0700

    ss

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - print strings reversed
+ * @s: The parameter "*s" represents the string
+ * Return: no return
+*/
+void print_rev(char *s)
+{
+int i;
+int count = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+count++;
+}
+for (i = count - 1; i >= 0; i--)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
