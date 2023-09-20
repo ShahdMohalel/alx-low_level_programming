8cde372dce041be425c5aff925920c3a79fda470
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-20 09:13:57 -0700

    ff

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+char *_strncat(char *dest, char *src, int n)
+{
+
+int src_count = 0;
+int dest_count = 0;
+int i;
+int concat_size;
+char *concat;
+for (i = 0; src[i] != '\0'; i++)
+{
+src_count++;
+}
+for (i = 0; dest[i] != '\0'; i++)
+{
+dest_count++;
+}
+concat_size = src_count + dest_count;
+concat = (char *)malloc((concat_size + 1) * sizeof(char));
+for (i = 0; i < dest_count; i++)
+{
+concat[i] = dest[i];
+}
+for (i = 0; i < n; i++)
+{
+concat[dest_count + i] = src[i];
+}
+return (concat);
+
+}
