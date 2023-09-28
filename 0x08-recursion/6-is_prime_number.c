0f0fd4775948a6e318784fafb53eee2db9b4b37e
ShahdMohalel <shahd.mohlal@gmail.com>
2023-09-28 13:46:15 -0700

    hh

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,6 +6,7 @@
 */
 int is_prime_number(int n)
 {
+int i;
 if (n <= 1)
 {
 return (0);
@@ -18,7 +19,7 @@ if (n % 2 == 0 || n % 3 == 0)
 {
 return (0);
 }
-for (int i = 5; i * i <= n; i += 6)
+for (i = 5; i * i <= n; i += 6)
 {
 if (n % i == 0 || n % (i + 2) == 0)
 {
