91053211ab06f7471e4cd622fabc19fe7c1b5184
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-03 05:53:56 -0700

    gg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print program name.
+ * @argc: command count.
+ * @argv: command array.
+ * Return: 0 always
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
