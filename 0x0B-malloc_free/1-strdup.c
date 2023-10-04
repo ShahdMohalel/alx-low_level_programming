7ecaf1cacdef462c26e2e156b4e5ee032296b182
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-04 13:19:32 -0700

    gg

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * _strdup -  returns a pointer to a newly allocated space in memory.
  * @str: The parameter "str" represents the string.
