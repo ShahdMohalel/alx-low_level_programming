41bda447f9ba318f6e95919eae5df2c503ed95e6
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-04 12:20:33 -0700

    ff

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * create_array - create an array
+ * @c: The parameter 'c' represents the character.
+ * @size: The parameter "size" represents the size of the array.
+ * Return: pointer to ar.
+*/
+char *create_array(unsigned int size, char c)
+{
+int i;
+char *ar;
+ar = malloc(size * sizeof(char));
+for (i = 0; i < size; i++)
+{
+ar[i] = c;
+}
+return (ar);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Malloce and free
