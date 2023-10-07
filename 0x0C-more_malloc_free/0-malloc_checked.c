f19b59c1ea95fd85062fcbb5eb2f88daafb63f14
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-07 07:00:19 -0700

    gg

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,20 +4,13 @@
 #include <limits.h>
 void *malloc_checked(unsigned int b)
 {
-long unsigned int max_memory;
-int *arr;
-max_memory = (1024 * 1024 * 1024);
-if (sizeof(b) > 2 *  max_memory)
+void *arr;
+if (b >= INT_MAX)
 {
 exit(98);
 }
-arr = malloc(sizeof(b));
-
-if(arr == NULL)
-{
-exit(98);
-}
-if(b == INT_MAX)
+arr = malloc(b);
+if (arr == NULL)
 {
 exit(98);
 }
