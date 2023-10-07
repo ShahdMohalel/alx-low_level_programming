deaac96561fa61d672db4c211e9dc99eb1022282
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-07 09:01:47 -0700

    gg

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
 /**
@@ -12,6 +13,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *ptr;
 unsigned int i;
+if (nmemb * size >= INT_MAX)
+{
+return (NULL);
+}
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
