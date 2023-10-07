ebd93e5e82c3d9c006571a0bbcdf40519eaf1e71
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-07 12:45:47 -0700

    ff

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include "main.h"
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *new_ptr;
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL && new_size != 0)
+{
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+{
+return (NULL);
+}
+return new_ptr;
+}
+if (new_size > old_size)
+{
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+{
+return (NULL);
+}
+strcpy(new_ptr, ptr);
+free(ptr);
+return (new_ptr);
+}
+else
+{
+return (ptr);
+}
+return (NULL);
+}
