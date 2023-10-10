0be098a08448f8b96d02e580eb961b987b40ced1
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-10 08:57:27 -0700

    gg

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - initiat struct.
+ * @d: The parameter 'd' represents the dog struct
+ * @name: the name.
+ * @age: the age.
+ * @owner: the owner.
+ * Return: no return.
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d -> name = name;
+d -> age = age;
+d -> owner = owner;
+}
