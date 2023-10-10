52eec8f5a7fb83e8ef6de3ca0abb63badf3dc3b5
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-10 08:43:18 -0700

    gg

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - dogs structure.
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+#ifndef DOG
+#define DOG
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
