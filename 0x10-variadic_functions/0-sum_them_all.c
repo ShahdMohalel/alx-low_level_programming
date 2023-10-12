5b1e33af3fcf94c176ff3bd8af81dd45023c35e3
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-12 04:06:01 -0700

    gg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 int sum = 0;
-int i;
+unsigned int i;
+va_list ptr;
 if (n == 0)
 {
 return (0);
 }
-va_list ptr;
 va_start(ptr, n);
 for (i = 0; i < n; i++)
 {
