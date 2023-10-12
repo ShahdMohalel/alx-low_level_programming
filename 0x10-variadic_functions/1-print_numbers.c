f3e564474e10af6001262cb2d6c80c4af53c87a7
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-12 05:05:49 -0700

    gg

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ va_start(ptr, n);
 for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(ptr, int));
-if (i <= n - 1 && *separator != '\0')
+if (i < n - 1 && *separator != '\0')
 {
 printf("%s ", separator);
 }
