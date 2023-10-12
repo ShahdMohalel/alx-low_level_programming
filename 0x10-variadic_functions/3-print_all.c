f7f36334bff3454b2ae572b2aadf6d2fd01e7930
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-12 09:43:35 -0700

    gg

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+void print_all(const char * const format, ...)
+{
+va_list ptr;
+int i;
+va_start(ptr, format);
+if(format == NULL && *format == '\0')
+{
+return;
+}
+i = 0;
+while (format[i] != '\0')
+{
+switch (format[i])
+{
+case 'i':
+printf("%d, ", va_arg(ptr, int));
+break;
+case 'f':
+printf("%f, ", va_arg(ptr, double));
+break;
+case 's':
+printf("%s", va_arg(ptr, char *));
+break;
+case 'c':
+printf("%c, ", va_arg(ptr, int));
+default:
+break;
+}
+i++;
+}
+va_end(ptr);
+printf("\n");
+}
