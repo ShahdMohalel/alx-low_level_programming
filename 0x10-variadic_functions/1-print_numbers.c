541d567dc1700d0f5356b451a56c9f3e28da0c8e
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-12 04:47:49 -0700

    gg

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdlib.h>
+/**
+ * print_numbers - print numbers.
+ * @n: The parameter 'n' represents the number of integrs
+ * @separator: the ,
+ * Return: no return.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list ptr;
+va_start(ptr, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ptr, int));
+if (i != n - 1)
+{
+printf("%c ", separator[0] == NULL ? "" : separator[0]);
+}
+else
+{
+printf("");
+}
+}
+printf("\n");
+}
