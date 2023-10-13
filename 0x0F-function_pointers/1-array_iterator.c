db97cbe1402ff3131776274f5d2d05daf10c5195
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-12 23:52:08 -0700

    gg

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * array_iterator - action on array
+ * @array: The array.
+ * @size: array size.
+ * @action: function pointer.
+ * Return: 1 if true / 0 if false
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+if (array != NULL && action != NULL)
+{
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+}
