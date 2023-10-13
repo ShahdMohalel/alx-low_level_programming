dc04bb622c7d7765a2355885065536516c38db8d
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-12 23:36:47 -0700

    gg

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_name - prints name.
  * @name: name.
@@ -7,7 +8,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == '\0')
+if (name == NULL)
 {
 return;
 }
