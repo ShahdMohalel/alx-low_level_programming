63d85b03d2bc02cbff0eaec53cd36dc2854277ca
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-19 13:35:28 -0700

    gg

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - print linked list
+ * @h: pointer to the struct
+ * return: num. of nodes
+ */
+size_t print_list(const list_t *h)
+{
+int l = 0;
+const list_t *current = h;
+while (current != NULL)
+{
+if (current->str == NULL)
+{
+printf("[0] (nil)\n");
+l++;
+}
+else
+{
+printf("[%u] %s\n", current->len, current->str);
+l++;
+}
+current = current->next;
+}
+return (l);
+}
