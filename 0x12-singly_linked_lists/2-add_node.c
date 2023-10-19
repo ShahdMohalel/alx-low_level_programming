1c146b89234c8fc6cac2474aba1467fe80077887
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-19 14:30:01 -0700

    gg

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_node - print linked list
+ * @head: pointer to the struct
+ * @str: string.
+ * Return: address to new node of.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new_head;
+new_head = malloc(sizeof(list_t));
+if (new_head == NULL)
+{
+return (NULL);
+}
+if (str == NULL)
+{
+return (NULL);
+}
+if (head == NULL)
+{
+return (NULL);
+}
+new_head->str = strdup(str);
+new_head->next = *head;
+*head = new_head;
+return (new_head);
+}
