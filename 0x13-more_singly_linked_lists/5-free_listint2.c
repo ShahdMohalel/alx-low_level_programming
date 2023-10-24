1efdf1451e1c5e34f7833a4d5b4316c3632a30f7
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-24 05:55:40 -0700

    gg

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - add end node
+ * @head: pointer to the struct
+ * @n: num.
+ * Return: address to new node of.
+ */
+void free_listint2(listint_t **head)
+{
+*head = NULL;
+}
