d111052743496d229b8b04ca5f725eb7da519fee
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-24 06:35:16 -0700

    hh

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - delete a node
+ * @head: pointer to the struct
+ * Return: deleted num.
+ */
+int pop_listint(listint_t **head)
+{
+int del_num;
+listint_t *current;
+if (head == NULL || *head == NULL)
+{
+return (0);
+}
+*head = current;
+del_num = current->n;
+*head = current->next;
+free(current);
+return (del_num);
+}
