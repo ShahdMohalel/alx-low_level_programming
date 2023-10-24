fbb993badc9c513330c164a0dfc162ca13c56c2d
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-24 04:38:00 -0700

    gg

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,17 @@
+#include "lists.h"
 /**
  * print_listint - print linked list
  * @h: pointer to the struct
  * Return: num. of nodes
  */
 size_t print_listint(const listint_t *h)
+{
 int len = 0;
 int i;
 if (h == NULL)
 {
 return (0);
-}    
+}
 printf("%d\n", h->n);
 len++;
 len += print_listint(h->next);
