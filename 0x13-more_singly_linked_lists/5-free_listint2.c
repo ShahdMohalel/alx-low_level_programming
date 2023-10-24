d0705f293f507a2f775b70628dc122087dc24c7e
ShahdMohalel <shahd.mohlal@gmail.com>
2023-10-24 06:10:57 -0700

    gg

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,15 @@
 void free_listint2(listint_t **head)
 {
 listint_t *current;
+if (head == NULL || *head == NULL)
+{
+return;
+}
 while (*head != NULL)
 {
 current = *head;
-*head = head->next;
+*head = current->next;
 free(current);
 }
+*head = NULL;
 }
